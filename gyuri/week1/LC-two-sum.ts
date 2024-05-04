b2348965cebe144e6b772c7f2f637601df8fb82d
devGyulMem <khmdevmem@gmail.com>
2024-05-04 22:23:51 +0900

    Feat: Week1

---
+++ gyuri/week1/LC-two-sum.ts
@@ -0,0 +1,10 @@
+// 2중 for문으로 풀었을 때
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
