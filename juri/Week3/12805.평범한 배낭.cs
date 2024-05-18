4f78bd5d9cf2f3a9b14d01d72bf3dcaa5f627703
jeongyeon-park <dus7653@gmail.com>
2024-05-18 18:27:43 +0900

    Fix : 평범한 배낭문제 수정

--- juri/Week3/12805.평범한 배낭.cs
+++ juri/Week3/12805.평범한 배낭.cs
@@ -8,8 +8,7 @@ namespace BOJ
             int n = int.Parse(nums[0]);
             int limit = int.Parse(nums[1]);
             int[,] list = new int[n, 2];
-            int[] result = new int[n + 1]; // n+1 크기로 변경
-            Array.Fill(result, 0);
+            int[,] dp = new int[n + 1, limit + 1];
 
             for (int i = 0; i < n; i++)
             {
@@ -18,29 +17,20 @@ namespace BOJ
                 list[i, 1] = int.Parse(str[1]); // Value
             }
 
-            int sumW = 0;
-            for (int j = n - 1; j >= 0; j--)
+            for (int i = 1; i <= n; i++)
             {
-                int currentW = list[j, 0];
-                int currentV = list[j, 1];
-                if (limit >= currentW + sumW)
+                int weight = list[i - 1, 0];
+                int value = list[i - 1, 1];
+                for (int j = 0; j <= limit; j++)
                 {
-                    if (j != 0) // 마지막 요소가 아니라면
-                    {
-                        int lastW = list[j - 1, 0];
-                        int lastV = list[j - 1, 1];
-
-                        if (lastW + currentW <= limit && result[j + 1] < lastV + currentV)
-                        {
-                            sumW = currentW + lastW;
-                            result[j] = lastV + currentV;
-                            continue;
-                        }
-                    }
+                    if (j < weight)
+                        dp[i, j] = dp[i - 1, j];
+                    else
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i - 1, j - weight] + value);
                 }
-                result[j] = result[j + 1];
             }
-            Console.WriteLine(result[0]);
+
+            Console.WriteLine(dp[n, limit]);
         }
     }
 }
